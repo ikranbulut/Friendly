25703f821fcc5c10c2aa2755687b7c98016848f2
İkranur <ikranur.bulut@mellon.com.tr>
2021-09-02 16:15:12 +0300

    Initial Commit

---
+++ Friendly/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Friendly
+//
+//  Created by Mac on 2.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
