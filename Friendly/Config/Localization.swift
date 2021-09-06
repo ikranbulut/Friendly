02b857a6b47220b06d44deb221a394d72e466418
İkranur <ikranur.bulut@mellon.com.tr>
2021-09-06 14:01:16 +0300

    Localization added

---
+++ Friendly/Config/Localization.swift
@@ -0,0 +1,19 @@
+//
+//  Localization.swift
+//  Friendly
+//
+//  Created by Mac on 6.09.2021.
+//
+
+import Foundation
+
+enum Localization {
+  enum Friend {
+    static let fullname = "friend.fullname".localized()
+    static let gender = "friend.gender".localized()
+    static let country = "friend.country".localized()
+    static let email = "friend.email".localized()
+    static let age = "friend.age".localized()
+    static let phoneNumber = "friend.phoneNumber".localized()
+  }
+}
